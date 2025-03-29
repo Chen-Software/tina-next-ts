00ba912f8ad570b1481dd7e3fc2e8e47941473c3
Yumin Chen <yumin@chen.so>
2025-03-29 14:23:04 +0000

    Field component
    - Issue: https://git.chen.so/tina-next-ts/i/80a9c37b9191b46d18f7b9a984acfc05032ff646

---
+++ app/components/Field/index.tsx
@@ -0,0 +1,64 @@
+import React, { createElement as h } from "react";
+import { Field } from "./field";
+
+const FieldBase = React.forwardRef<Field.RootProps>(
+	(
+		{
+			children,
+			label,
+			placeholder,
+			helperText = undefined,
+			errorText = undefined,
+			...props
+		},
+		ref,
+	) => {
+		return (
+			<Field.Root ref={ref} {...props}>
+				<Field.Label>{label}</Field.Label>
+				{children}
+				{helperText && <Field.HelperText>{helperText}</Field.HelperText>}
+				{errorText && <Field.ErrorText>{errorText}</Field.ErrorText>}
+			</Field.Root>
+		);
+	},
+);
+
+FieldBase.displayName = "Field";
+
+export const InputField = ({
+	multiLine = false,
+	type = "text",
+	placeholder = "Input here…",
+	asChild = false,
+	...props
+}) => (
+	<FieldBase {...props}>
+		{h(
+			multiLine ? Field.Textarea : Field.Input,
+			{
+				placeholder,
+				type,
+				asChild,
+			},
+			(asChild && props.children) || undefined,
+		)}
+	</FieldBase>
+);
+
+export const TextField = InputField;
+
+export const SelectField = ({ options, ...props }) => (
+	<FieldBase {...props}>
+		<Field.Select>
+			{options.map((option) => (
+				<option key={option.value} value={option.value}>
+					{option.label}
+				</option>
+			))}
+		</Field.Select>
+	</FieldBase>
+);
+
+export type FieldProps = Field.RootProps;
+export default InputField;
