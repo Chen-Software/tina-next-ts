70c64ea92ae9e774014c740e6dcaaca73efa42fd
Yumin Chen <yumin@chen.so>
2025-03-31 19:53:22 +0100

    Support `tooltip` prop in `Button` component
    - Issue: https://git.chen.so/tina-next-ts/i/79e6f1e7778843269ef7ec34437c97b32e34f621

---
+++ app/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import { Tooltip } from "../Tooltip";
+import { Button as ButtonBase, type ButtonProps } from "./button";
+
+export const Button = ({ tooltip, children, props }: ButtonProps) => {
+	if (tooltip) {
+		return (
+			<Tooltip
+				trigger={<ButtonBase {...props}>{children}</ButtonBase>}
+				content={tooltip}
+			/>
+		);
+	}
+
+	return <ButtonBase {...props}>{children}</ButtonBase>;
+};
+
+export * from "./button";
