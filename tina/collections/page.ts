11fd89a31f4479e4811d46adcd8aced1cb315410
Yumin Chen <yumin@chen.so>
2025-04-01 19:52:03 +0100

    Update page collection schema to include Text
    - Issue: https://git.chen.so/tina-next-ts/i/a73c55cc74b064c88d2555cad0e1777b8692b7c7

--- tina/collections/page.ts
+++ tina/collections/page.ts
@@ -1,4 +1,5 @@
 import { heading } from "app/components/Heading/heading.template";
+import { text } from "app/components/Text/text.template";
 import { button } from "../../app/components/Button/button.template";
 
 /**
@@ -16,7 +17,7 @@ export default {
 			type: "object",
 			list: true,
 			isBody: true,
-			templates: [button, heading],
+			templates: [button, heading, text],
 		},
 	],
 	ui: {
