d63961336e442dd7aa44c785e1179790489c6e6d
Yumin Chen <yumin@chen.so>
2025-04-01 19:49:03 +0100

    Text template for CMS
    - Issue: https://git.chen.so/tina-next-ts/i/a73c55cc74b064c88d2555cad0e1777b8692b7c7

---
+++ app/components/Text/text.template.ts
@@ -0,0 +1,139 @@
+import type { Template } from "tinacms";
+
+export const text: Template = {
+	name: "text",
+	label: "Text",
+	fields: [
+		{
+			type: "string",
+			label: "Type",
+			name: "type",
+			options: [
+				{
+					value: "inline",
+					label: "Inline",
+				},
+				{
+					value: "block",
+					label: "Block",
+				},
+			],
+		},
+		{
+			type: "string",
+			label: "Content",
+			name: "content",
+			description: "Main content.",
+		},
+		{
+			type: "string",
+			label: "Font Size",
+			name: "size",
+			description:
+				"Text font size. It makes line height and spacing smaller as text size grows and also ensures text size is even for better layout.",
+			options: [
+				{
+					value: "xs",
+					label: "Extra Small",
+				},
+				{
+					value: "sm",
+					label: "Small",
+				},
+				{
+					value: "md",
+					label: "Medium",
+				},
+				{
+					value: "lg",
+					label: "Large",
+				},
+				{
+					value: "xl",
+					label: "Extra Large",
+				},
+				{
+					value: "2xl",
+					label: "2 Extra Large",
+				},
+				{
+					value: "3xl",
+					label: "3 Extra Large",
+				},
+				{
+					value: "4xl",
+					label: "4 Extra Large",
+				},
+				{
+					value: "5xl",
+					label: "5 Extra Large",
+				},
+				{
+					value: "6xl",
+					label: "6 Extra Large",
+				},
+				{
+					value: "7xl",
+					label: "7 Extra Large",
+				},
+			],
+		},
+		{
+			type: "string",
+			label: "Font Weight",
+			name: "fontWeight",
+			description: "Text font weight.",
+			options: [
+				{
+					value: "light",
+					label: "Light",
+				},
+				{
+					value: "normal",
+					label: "Normal",
+				},
+				{
+					value: "medium",
+					label: "Medium",
+				},
+				{
+					value: "semibold",
+					label: "Semi-Bold",
+				},
+				{
+					value: "bold",
+					label: "Bold",
+				},
+			],
+		},
+		{
+			type: "object",
+			label: "Advanced Configurations",
+			name: "advancedConfigs",
+			description: "Additional configurations for the text.",
+			ui: {
+				component: "group",
+			},
+			fields: [
+				{
+					type: "object",
+					label: "Accessibility",
+					name: "accessibility",
+					description: "Accessibility options for the text.",
+					ui: {
+						component: "group",
+					},
+					fields: [
+						{
+							type: "number",
+							label: "Level (ARIA)",
+							name: "ariaLevel",
+							description:
+								"The hierarchical level of the heading within a structure, when the text represents a heading. Levels increase with depth. The value is an integer greater than or equal to 1. Leave empty if the text is not a heading.",
+						},
+					],
+				},
+			],
+		},
+	],
+};
