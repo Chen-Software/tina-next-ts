e2fc4e9547f78d8e5ba4c2acaee3bd90140fc8c8
Yumin Chen <yumin@chen.so>
2025-04-07 10:19:36 +0100

    Export DialogProps for Dialog CMS component
    - Issue: https://git.chen.so/tina-next-ts/i/b37b578a9651d8144d5ca5e67a2947c99aeb13e0

--- app/components/Dialog/dialog.cms.tsx
+++ app/components/Dialog/dialog.cms.tsx
@@ -1,6 +1,24 @@
 import { createElement as h } from "react";
 import { Dialog as DialogBase } from ".";
-import { Button } from "../Button/button.cms";
+import { Button, type ButtonProps } from "../Button/button.cms";
+
+export type DialogProps = {
+	__typename: "PageBodyDialog";
+	title?: string | null | undefined;
+	description?: string | null | undefined;
+	confirmButton?: ButtonProps;
+	cancelButton?: ButtonProps;
+	closeButton?: ButtonProps;
+	trigger?: ButtonProps;
+	advancedConfigs?: {
+		defaultOpen?: boolean | null | undefined;
+		lazyMount?: boolean | null | undefined;
+		unmountOnExit?: boolean | null | undefined;
+		accessibility?: {
+			ariaLabel?: string | null | undefined;
+		};
+	};
+};
 
 export const Dialog = ({
 	__typename,
@@ -10,7 +28,7 @@ export const Dialog = ({
 	trigger,
 	advancedConfigs,
 	...props
-}) => {
+}: DialogProps) => {
 	const { accessibility, ...advancedConfigurations } = advancedConfigs || {};
 
 	return h(DialogBase, {
