03f3076edbdf13604bce866678f4df70bedb36ca
Yumin Chen <yumin@chen.so>
2025-04-08 11:10:42 +0100

    Link template for CMS
    - Issue: https://git.chen.so/tina-next-ts/i/7be45a0724401d6aa8b5fb5bcd8dae3f29f893c0

--- tina/collections/page.ts
+++ tina/collections/page.ts
@@ -1,5 +1,6 @@
 import { dialog } from "app/components/Dialog/dialog.template";
 import { heading } from "app/components/Heading/heading.template";
+import { link } from "app/components/Link/link.template";
 import { paragraph } from "app/components/Text/paragraph.template";
 import { text } from "app/components/Text/text.template";
 import { button } from "../../app/components/Button/button.template";
@@ -19,7 +20,7 @@ export default {
 			type: "object",
 			list: true,
 			isBody: true,
-			templates: [button, heading, text, paragraph, dialog],
+			templates: [button, heading, text, link, paragraph, dialog],
 		},
 	],
 	ui: {
