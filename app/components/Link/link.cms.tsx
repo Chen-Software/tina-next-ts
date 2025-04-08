3d1f1c25d40cac82e9019c16afc6f849cdca2f88
Yumin Chen <yumin@chen.so>
2025-04-08 11:53:47 +0100

    Fix href type error
    - Issue: https://git.chen.so/tina-next-ts/i/7be45a0724401d6aa8b5fb5bcd8dae3f29f893c0

--- app/components/Link/link.cms.tsx
+++ app/components/Link/link.cms.tsx
@@ -7,12 +7,14 @@ export type LinkProps = {
 	key?: string;
 	type?: string | null;
 	label?: string | null;
+	href?: string | null;
 	content?: string | null;
 };
 
 export const Link = ({ __typename, type, label, ...props }: LinkProps) => {
 	return h(TextBase, {
-		as: ({ children, ...p }) => h(LinkBase, { as: type, ...p }, children),
+		as: ({ children, href, ...p }) =>
+			h(LinkBase, { as: type, href: href || "", ...p }, children),
 		content: label,
 		...props,
 	});
